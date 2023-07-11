2d8fee5b91feac9cc1aa3cb7f28398a5cb8c09af
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-07-11 16:36:18 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
-from socket import socketClass
-
+from socket import *;
 
 HOST = '172.18.64.1';
 PORT = 55000;
 
+            #IPV4 enabled,  TCP enabled
+sok = socket( AF_INET, SOCK_STREAM);
+sok.connect( (HOST, PORT) );
+
+sok.sendall( str.encode( "Hello my friend!!!", encoding="utf-8") );
+
+response = sok.recv(1024);
+print("Server response: ", response.decode() );

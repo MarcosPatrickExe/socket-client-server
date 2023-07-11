76cdc1897f5f9251b074e7c5bc83462c4c87b03f
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-07-11 17:46:31 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,21 @@
 from socket import *;
 
-HOST = '172.18.64.1';
-PORT = 55000;
+SERVER_NAME = gethostname();
+SERVER_PORT = 55000;
 
-            #IPV4 enabled,  TCP enabled
-sok = socket( AF_INET, SOCK_STREAM);
-sok.connect( (HOST, PORT) );
+        
+try:
+               #  IPV4 enabled, TCP enabled
+    sok = socket(    AF_INET,   SOCK_STREAM)
+    sok.connect( (gethostname(), SERVER_PORT) );        # The connect() func only receive a tuple 
+    sok.sendall( str.encode( "Hello my friend!!!", encoding="utf-8") );
 
-sok.sendall( str.encode( "Hello my friend!!!", encoding="utf-8") );
+    response = sok.recv(1024);
+    print("Server response: ", response.decode() );
 
-response = sok.recv(1024);
-print("Server response: ", response.decode() );
+
+except TimeoutError as timeErr:
+    print("\n \n Tempo de conexão com o servidor excedida... ", timeErr);
+finally:
+    print("\n Encerrando socket de configuracao do cliente TCP....");
+    sok.close();

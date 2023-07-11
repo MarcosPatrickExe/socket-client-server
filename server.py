d2290d8360346c8d085432a5534a7080501f32bd
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-07-11 17:47:13 -0300

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import logging;
 
 HOST = socket.gethostname();
 PORT = 55000;
+TIMEOUT = 20.0;
 
 print("Server IP: ", socket.gethostbyname(HOST)," // Server name: ", HOST);
 
@@ -12,34 +13,34 @@ print("Server IP: ", socket.gethostbyname(HOST)," // Server name: ", HOST);
 socketConfig = socket.socket( socket.AF_INET, socket.SOCK_STREAM );
 socketConfig.bind((HOST, PORT)); # vinculando HOST and PORT
 socketConfig.listen(); # stop the code here until receive a new connection
-socketConfig.settimeout(15.0)
-print("Server is waiting connection..... time left: 15 seconds..."); 
+socketConfig.settimeout(TIMEOUT)
+print("Server is waiting connection..... time left: ",TIMEOUT," seconds... \n"); 
 
 
 try:
     # Here below only executes when client has been conected:
     clientSocket, clientAddr = socketConfig.accept(); # accept return a tuple()  with 2 variables
-    print("client connected!  address: ", clientAddr[0], "/ port: ", clientAddr[1] );
+    print("\n client connected!  address: ", clientAddr[0], "/ port: ", clientAddr[1] );
+
 
     while(True):
-        print("listenning while.....")
-        message = clientSocket.recv(1024); # 1024 byte will be receive from client
+        message = clientSocket.recv(1024);  # 1024 bytes will be receive from client
     
         if not message:
-            print("NOT DATA FOUND !" );
-            clientSocket.close();
+            print("\n NOT DATA FOUND !" );
+            clientSocket.close();           # the 'clientSocket' oject will be empty now...
             break;
         else:
-            print("Client message: ", message.decode());
+            print("\n Client message: ", message.decode());
             print("send all message to clients sockets...." );
             clientSocket.sendall(message);
 
 
 except timeout as timeoutError:
-    print("Timeout expirado!");
+    print("\n \nTimeout expirado!");
     logging.error(msg= timeoutError);
 
 finally:
-    print("Encerrando socket de configuracao do servidor socket TCP....");
+    print("\nEncerrando socket de configuracao do servidor TCP....");
     socketConfig.close();
 

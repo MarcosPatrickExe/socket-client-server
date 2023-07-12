7bc6ba19c0c552db72eb567994ac552f33d467b6
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-07-11 22:08:17 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,46 +1,63 @@
 import socket;
 from socket import timeout;
 import logging;
+from threading import Thread;
 
 
+clients = []
 HOST = socket.gethostname();
 PORT = 55000;
 TIMEOUT = 20.0;
 
-print("Server IP: ", socket.gethostbyname(HOST)," // Server name: ", HOST);
+print("\nServer IP: ", socket.gethostbyname(HOST)," // Server name: ", HOST);
 
-                   # IPV4 enabled,  TCP enabled
-socketConfig = socket.socket( socket.AF_INET, socket.SOCK_STREAM );
-socketConfig.bind((HOST, PORT)); # vinculando HOST and PORT
-socketConfig.listen(); # stop the code here until receive a new connection
-socketConfig.settimeout(TIMEOUT)
+
+def clientThread( clientSocket):
+    while True:
+        try:
+            # Esperando nova resposta para executar as linhas abaixo....
+            message = clientSocket.recv(2048);  # 1024 bytes will be receive from client
+            broadcastMessage( message);
+            print("\n", message.decode());
+            # clientSocket.sendall(message.encode());
+        except:
+            print("\n \n => erro na thread do client ao tentar receber mensagem.....");
+            clients.remove(clientSocket);
+            return; #ou use 'break;'
+
+
+def broadcastMessage( newMessage):
+    for clientItem in clients:
+      # if clientItem != clientCurrent
+        try:
+            clientItem.send( newMessage );
+        except Exception as exc:
+            print("\n \n => Erro ao enviar mensagens para os clients conectados... info: ", exc);
+            clients.remove(clientItem);
+
+
+
+
+                # IPV4 enabled,  TCP enabled
+server = socket.socket( socket.AF_INET, socket.SOCK_STREAM );
+server.bind((HOST, PORT)); # vinculando HOST and PORT
+server.listen(); # stop the code here until receive a new connection
+server.settimeout(TIMEOUT)
 print("Server is waiting connection..... time left: ",TIMEOUT," seconds... \n"); 
 
-
 try:
-    # Here below only executes when client has been conected:
-    clientSocket, clientAddr = socketConfig.accept(); # accept return a tuple()  with 2 variables
-    print("\n client connected!  address: ", clientAddr[0], "/ port: ", clientAddr[1] );
-
-
-    while(True):
-        message = clientSocket.recv(1024);  # 1024 bytes will be receive from client
-    
-        if not message:
-            print("\n NOT DATA FOUND !" );
-            clientSocket.close();           # the 'clientSocket' oject will be empty now...
-            break;
-        else:
-            print("\n Client message: ", message.decode());
-            print("send all message to clients sockets...." );
-            clientSocket.sendall(message);
-
+    while True:
+        # A linha abaixo somente eh executada quando um novo socket se conecta...
+        clientSocket, clientAddr = server.accept(); # accept return a tuple()  with 2 variables
+        print("\n client connected!  address: ", clientAddr[0], "/ port: ", clientAddr[1] );
+        clients.append( clientSocket);
+        thread = Thread(target =clientThread, args=[clientSocket]);
+        thread.start();
 
 except timeout as timeoutError:
     print("\n \nTimeout expirado!");
     logging.error(msg= timeoutError);
 
 finally:
-    print("\nEncerrando socket de configuracao do servidor TCP....");
-    socketConfig.close();
-
+    print("\nNão foi possível manter o servidor executando. Encerrando socket de configuracao do servidor TCP....");
+    server.close();
